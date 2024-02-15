a5d8bf168d4d5e22d291e5daa2cd6e8486f730a4
Angelica <Angelica137@users.noreply.github.com>
2024-02-15 08:41:33 -0600

    configure babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,20 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset"],
+          },
+        },
+      },
+    ],
   },
 };
