1ea81a96b1b5a56463df7729ed9e18cb6f6d6288
Angelica <Angelica137@users.noreply.github.com>
2024-02-16 10:10:28 -0600

    add js for auto complete - search and suggest functins

---
+++ src/client/index.js
@@ -0,0 +1,29 @@
+const { response } = require("express");
+
+function searchCities() {
+  const input = document.getElementById("location").value;
+
+  if (input.length < 3) return;
+
+  const username = GEONAMES_USERNAME;
+  const url = `http://api.geonames.org/searchJSON?formatted=true&q=${input}&maxRows=10&username=${username}&style=full&cities=cities1000`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      showSuggestions(data.geonames);
+    })
+    .catch((error) => console.error("Error fetching data: ", error));
+}
+
+function showSuggestions(geonames) {
+  const showSuggestionsList = document.getElementById("suggestions");
+  showSuggestionsList.innerHTML = "";
+
+  geonames.forEach((place) => {
+    const option = document.createElement("option");
+    option.value = place.name;
+    showSuggestionsList.appendChild(option);
+  });
+}
